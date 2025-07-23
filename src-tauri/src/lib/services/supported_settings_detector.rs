1f857959a1a11a4a302049eaceaac4bcd45f2331
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-07-23 17:36:49 -0300

    Make get_supported_settings async and update logic

--- src-tauri/src/lib/services/supported_settings_detector.rs
+++ src-tauri/src/lib/services/supported_settings_detector.rs
@@ -1,8 +1,17 @@
-use crate::{services::system_manager::SystemManager, BATTERY_SYSFS_PATH};
+use crate::{
+    services::{battery_thereshold_controller, system_manager::SystemManager},
+    BATTERY_SYSFS_PATH, PERFORMANCE_MODE_CHOICES_SYSFS_PATH,
+};
 
-pub fn get_supported_settings() -> Result<String, Box<dyn std::error::Error>> {
+pub async fn get_supported_settings() -> Result<String, Box<dyn std::error::Error>> {
     let sysfs = SystemManager::get_sysfs_manager()
         .map_err(|e| format!("Failed to get sysfs manager: {}", e))?;
 
-    let battery_threshold = sysfs.get_value(BATTERY_SYSFS_PATH);
+    let battery_threshold = sysfs.get_value(BATTERY_SYSFS_PATH).await.is_ok();
+    let performance_mode = sysfs
+        .get_value(PERFORMANCE_MODE_CHOICES_SYSFS_PATH)
+        .await
+        .is_ok();
+
+    Ok("Pra parar de dar erro".to_string())
 }
