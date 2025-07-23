292e7de3f0194a02651bebc3b2a4e64303d61892
Pedro Andrade Giroldo <pedrogiroldo40@gmail.com>
2025-07-23 17:46:42 -0300

    Detect and report support for new sysfs settings

--- src-tauri/src/lib/mod.rs
+++ src-tauri/src/lib/mod.rs
@@ -8,6 +8,9 @@ use crate::services::system_manager;
 pub const BATTERY_SYSFS_PATH: &str = "/sys/class/power_supply/BAT1/charge_control_end_threshold";
 pub const PERFORMANCE_MODE_SYSFS_PATH: &str = "/sys/firmware/acpi/platform_profile";
 pub const PERFORMANCE_MODE_CHOICES_SYSFS_PATH: &str = "/sys/firmware/acpi/platform_profile_choices";
+pub const POWER_ON_LID_DIRECTORY_PATH: &str = "/sys/class/power_supply/BAT1/power_on_lid";
+pub const USB_CHARGING_DIRECTORY_PATH: &str = "/sys/class/power_supply/BAT1/usb_charging";
+pub const BLOCK_RECORDING_DIRECTORY_PATH: &str = "/sys/class/power_supply/BAT1/block_recording";
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
--- src-tauri/src/lib/services/supported_settings_detector.rs
+++ src-tauri/src/lib/services/supported_settings_detector.rs
@@ -1,6 +1,6 @@
 use crate::{
-    services::{battery_thereshold_controller, system_manager::SystemManager},
-    BATTERY_SYSFS_PATH, PERFORMANCE_MODE_CHOICES_SYSFS_PATH,
+    services::system_manager::SystemManager, BATTERY_SYSFS_PATH, BLOCK_RECORDING_DIRECTORY_PATH,
+    PERFORMANCE_MODE_CHOICES_SYSFS_PATH, POWER_ON_LID_DIRECTORY_PATH, USB_CHARGING_DIRECTORY_PATH,
 };
 
 pub async fn get_supported_settings() -> Result<String, Box<dyn std::error::Error>> {
@@ -12,6 +12,33 @@ pub async fn get_supported_settings() -> Result<String, Box<dyn std::error::Erro
         .get_value(PERFORMANCE_MODE_CHOICES_SYSFS_PATH)
         .await
         .is_ok();
+    let power_on_lid = sysfs
+        .ls_directory(POWER_ON_LID_DIRECTORY_PATH)
+        .await
+        .is_ok();
+    let usb_charging = sysfs
+        .ls_directory(USB_CHARGING_DIRECTORY_PATH)
+        .await
+        .is_ok();
+    let block_recording = sysfs
+        .ls_directory(BLOCK_RECORDING_DIRECTORY_PATH)
+        .await
+        .is_ok();
 
-    Ok("Pra parar de dar erro".to_string())
+    let supported_settings = vec![
+        ("battery_threshold", battery_threshold),
+        ("performance_mode", performance_mode),
+        ("power_on_lid", power_on_lid),
+        ("usb_charging", usb_charging),
+        ("block_recording", block_recording),
+    ];
+
+    let supported_settings_json: Vec<_> = supported_settings
+        .into_iter()
+        .map(|(name, is_supported)| (name.to_string(), is_supported))
+        .collect();
+    let json = serde_json::to_string(&supported_settings_json)
+        .map_err(|e| format!("Failed to serialize supported settings: {}", e))?;
+
+    Ok(json)
 }
